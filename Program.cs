cb97769a39da5737a527b8cdd40db83920040066
prdb <prdb@spb-rtk.ru>
2023-10-18 16:16:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿//метод, двум массив, цикл
+using System;
+namespace Tests
+{
+    class Program
+    {
+        static void Main()
+        {
+            int e, h, a, t, d;
+            Console.WriteLine("Игра Быки и Коровы");
+            int[,] array = new int[2, 2];
+            Random random = new Random();
+            int[] mass = new int[4];
+            int z = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    array[i, j] = random.Next(0, 10);
+                    mass[z] = array[i, j];
+                    z++;
+                }
+            }
+            int attempts = 0;
+            bool guessed = false;
+
+            while (!guessed && attempts < 10)
+            {
+                Console.WriteLine($"Попытка {attempts + 1}: ");
+                Rand rnrn = new Rand();
+                int[] usernumber = rnrn.Out();
+                for (int i = 0; i < usernumber.Length; i++)
+                {
+
+                }
+                int korova = 0;
+                int bik = 0;
+                for (int i = 0; i < 4; i++)
+                    {
+                        if (usernumber.Contains(mass[i]))
+                        {
+                        if (usernumber[i] == mass[i])
+                        { bik++; }
+                        else
+                        { korova++; }
+                        }
+                    }
+                
+                Console.WriteLine($"Быки: {bik}, Коровы: {korova}");
+                attempts++;
+                if (bik == 4)
+                {
+                    guessed = true;
+                    Console.WriteLine("Вы угадали число!");
+                }
+
+               
+
+            }
+                if (!guessed)
+                {
+                    Console.Write("Вы не угадали число. Ваше число ");
+                for (int r = 0; r < mass.Length; r++)
+                {
+                    Console.Write(mass[r]);
+                }
+                }
+            }
+        }
+    }
+
---
+++ Rand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+     public class Rand
+    {
+        private int[] chislo;
+        public int[] Out()
+        {
+            int e, h, a, t, d;
+            int nn = int.Parse(Console.ReadLine());
+            string s = nn.ToString();
+            if (s.Length != 4)
+            {
+                Console.WriteLine("Вы неверно ввели число");
+                Environment.Exit(0);
+            }
+            
+            this.chislo = new int[4];
+            t = (nn / 1000);
+            h = (nn % 1000 / 100);
+            d = (nn % 100 / 10);
+            e = (nn % 10);
+            for (int o = 0; o < chislo.Length; o++)
+            {
+                if (o == 0)
+                {
+                    chislo[o] = t;
+                }
+                if (o == 1)
+                {
+                    chislo[o] = h;
+                }
+                if (o == 2)
+                {
+                    chislo[o] = d;
+                }
+                if (o == 3)
+                    chislo[o] = e;
+            }
+            return this.chislo;
+        }
+    }
+}
